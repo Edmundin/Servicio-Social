85a0a18f026f68ee6a99bec643ec2b7cc88f74ff
Edmundin <aedmundohm@ciencias.unam.mx>
2022-11-16 10:14:14 -0600

    Yap

--- Datos/Subirdatos.py
+++ Datos/Subirdatos.py
@@ -1,6 +1,5 @@
 
 import yfinance as yf
-from pathlib import Path
 from datetime import date, timedelta, datetime
 import time
 import pymysql
@@ -11,7 +10,8 @@ conn = pymysql.connect(
     host = "localhost",
     user = "root",
     password = "Cronopios_1",
-    database = "INDICES"
+    database = "INDICES", 
+    charset = "utf8"
 )
 
 cursor = conn.cursor() 
@@ -30,23 +30,33 @@ ticketsname = ["SP500", "Nasdaq", "Nasdaqcomp", "DWJ", "IPC", "STOXX50", "Nikkei
 
 #corregir
 
-for i in range(7):
-    start = datetime.today().replace(hour = 0, minute = 0, second = 0 ) - timedelta(days= i +1)
-    end = datetime.today().replace(hour = 23) - timedelta(days = i +1)
+
+start = datetime.today().replace(hour = 0, minute = 0, second = 0 ) - timedelta(days= 1)
+end = datetime.today().replace(hour = 23) - timedelta(days = 1)
 
         #agregar avg de open, high, low, close
 
+"""
+for i in range(len(tickets)):
+    historial = tickets[i].history(start=start, end=end , interval="1m") #Obtener los tickets
+    #guardamos los elementos
+    filepath = Path('Info/{almacenamiento}/{name}_{date}.csv'.format(almacenamiento = ticketsname[i] ,name = ticketsname[i], date = start.strftime('%Y-%m-%d')))
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    #guardar gráfica
+    historial.to_csv(filepath, header=True, index=True)
+"""
 
-    for i in range(len(tickets)):
-        historial = tickets[i].history(start=start, end=end , interval="1m") #Obtener los tickets
-        #Elegimos el path en el que se guardarán los csv, queremos que vaya a una carpeta adecuada a cada indicador
-        filepath = Path('Info/{almacenamiento}/{name}_{date}.csv'.format(almacenamiento = ticketsname[i] ,name = ticketsname[i], date = start.strftime('%Y-%m-%d')))
-        filepath.parent.mkdir(parents=True, exist_ok=True)
-        #guardar gráfica
-        historial.to_csv(filepath, header=True, index=True)
+historial = IPC.history(start = start, end = end, interval = '1m')
+
+for row in historial:
+    sql = "INSERT INTO datos(ID_INDICES, DATE_TIME, OPEN_, HIGH, LOW, CLOSE_, VOLUME, DIVIDENS, STOCK_SPLITS) VALUES(3,?,?,?,?,?,?,?,?)", (row[0].row[1],row[2],row[3],row[4],row[5],row[6],row[7])
+    try:
+        cursor.execute(sql)
+        conn.commit()
+    except:
+        conn.rollback()
+
+conn.close()
 
 
-
-sql =
-"INSERT INTO datos(ID_INDICES, DATE_TIME, OPEN_, HIGH, LOW, CLOSE_, VOLUME; DIVIDENS, STOCK_SPLITS) VALUES  "
 
